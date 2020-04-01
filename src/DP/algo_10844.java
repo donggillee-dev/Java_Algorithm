54dc2cbbce0c462fa053d04ee7ef615b36961d17
giri <donggillee94@gmail.com>
2020-04-01 17:55:47 +0900

    algo_10844 restart!!

--- src/DP/algo_10844.java
+++ src/DP/algo_10844.java
@@ -1,4 +1,16 @@
 package DP;
 
+import java.io.*;
+
 public class algo_10844 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        bw.flush();
+        bw.close();
+        br.close();
+        return;
+    }
 }
