b6e286f75542d13abfdba9d44fecc294493052b7
giri <donggillee94@gmail.com>
2020-04-16 04:07:32 +0900

    algo_1654 solved!!.... cause of data range

--- src/Binary_Search/algo_1654.java
+++ src/Binary_Search/algo_1654.java
@@ -3,21 +3,20 @@ import java.io.*;
 import java.util.*;
 
 public class algo_1654 {
-    static int N, M, start = 1, end = 0, ans = -1;
-    static int[] Cable;
+    static int N, M;
+    static long start = 1, end = 0, ans = -1;
+    static long[] Cable;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer stk = new StringTokenizer(br.readLine());
-        int sum = 0;
         M = Integer.parseInt(stk.nextToken());
         N = Integer.parseInt(stk.nextToken());
-        Cable = new int[M];
+        Cable = new long[M];
 
         for(int i = 0; i < M; i++) {
             Cable[i] = Integer.parseInt(br.readLine());
-            sum += Cable[i];
         }
         Arrays.sort(Cable);
         end = Cable[M - 1];
@@ -33,13 +32,13 @@ public class algo_1654 {
         return;
     }
     private static void Search() {
-        int mid = start + (end - start) / 2;
+        long mid = (start + end) / 2;
         int cnt = 0;
         for(int i = 0; i < M; i++) {
             cnt += (Cable[i] / mid);
         }
 
-        if(cnt > N) {
+        if(cnt >= N) {
             start = mid + 1;
             if(ans < mid) ans = mid;
         } else {
