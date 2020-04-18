5fa272eeee6ffd2f8a7194b3fb181e14ae2ff71b
giri <donggillee94@gmail.com>
2020-04-18 15:53:01 +0900

    algo_2805 start!!

---
+++ src/Binary_Search/algo_2805.java
@@ -0,0 +1,32 @@
+package Binary_Search;
+import java.io.*;
+import java.util.*;
+
+public class algo_2805 {
+    static int N, M;
+    static int[] Trees;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        System.out.println(Integer.MAX_VALUE);
+//
+//        StringTokenizer stk = new StringTokenizer(br.readLine());
+//        N = Integer.parseInt(stk.nextToken());
+//        M = Integer.parseInt(stk.nextToken());
+//        Trees = new int[N];
+//
+//        stk = new StringTokenizer(br.readLine());
+//        for(int i = 0; i < N; i++) {
+//            Trees[i] = Integer.parseInt(stk.nextToken());
+//        }
+//
+        bw.flush();
+        bw.close();
+        br.close();
+        return;
+    }
+    private static void Cutting() {
+
+    }
+}
