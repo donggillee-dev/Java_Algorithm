01bb000c0cfe2cf44a15eadc69d313f62e1ee110
giri <donggillee94@gmail.com>
2020-04-19 11:02:41 +0900

    algo_1300 start!!

---
+++ src/Binary_Search/algo_1300.java
@@ -0,0 +1,18 @@
+package Binary_Search;
+import java.io.*;
+import java.util.*;
+
+public class algo_1300 {
+    static int N, M;
+    static int[] An = new int[1000000001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        bw.flush();
+        bw.close();
+        br.close();
+        return;
+    }
+}
