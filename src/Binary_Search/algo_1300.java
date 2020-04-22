557e2adc73b1dd0f652083d8e9d0081985a48741
giri <donggillee94@gmail.com>
2020-04-22 19:31:29 +0900

    algo_1300 solved!!!!!

--- src/Binary_Search/algo_1300.java
+++ src/Binary_Search/algo_1300.java
@@ -1,16 +1,31 @@
 package Binary_Search;
-import java.io.*;
-import java.util.*;
+        import java.io.*;
 
 public class algo_1300 {
-    static int N, M;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+        int start = 1, end = K, ans = 0;
 
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            int cnt = 0;
+            for(int i = 1; i <= N; i++) { // N * N 배열의 각 행은 i의 배수
+                cnt += Math.min(mid / i, N); //행 전채 원소 개수 or mid값을 i로 나눴을시 몫이 mid보다 작은 값들의 개수
+            }
+
+            if(cnt >= K) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        sb.append(ans).append("\n");
+        bw.write(String.valueOf(sb));
         bw.flush();
         bw.close();
         br.close();
--- src/Binary_Search/algo_2100.java
+++ src/Binary_Search/algo_2100.java
@@ -3,7 +3,6 @@ import java.io.*;
 import java.util.*;
 
 public class algo_2100 {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -11,42 +10,38 @@ public class algo_2100 {
         StringTokenizer stk = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(stk.nextToken()), C = Integer.parseInt(stk.nextToken());
-        int min, max, ans = 0;
         int[] Position = new int[N];
+        int min, max, ans = 0;
 
         for(int i = 0; i < N; i++) {
             Position[i] = Integer.parseInt(br.readLine());
         }
-
         Arrays.sort(Position);
-        min = 1; //집 사이 최소거리
-        max = Position[N - 1] - Position[0]; //집 사이 최대거래
+        min = 1;
+        max = Position[N - 1] - Position[0];
 
         while(min <= max) {
-            int mid = (min + max) / 2; //중간값 거리
-            int cnt = 1; //공유기는 시작점에 하나 설치했다고 생각
-            int point = Position[0]; //시작점에는 하나 설치했다고 생각
-
+            int mid = (min + max) / 2;
+            int cnt = 1;
+            int spot = Position[0];
             for(int i = 1; i < N; i++) {
-                int d = Position[i] - point;
-                if(d >= mid) { //설정한 거리값 mid보다 작으므로 현재 설치지점 point에 널고 cnt는 증가
+                int d = Position[i] - spot;
+                if(d >= mid) {
                     cnt++;
-                    point = Position[i];
+                    spot = Position[i];
                 }
             }
 
-            if(cnt < C) { //설치해야되는 갯수보다 덜 설치했으니까 간격 줄이자
+            if(cnt < C) {
                 max = mid - 1;
-            } else { //설치해야하는 갯수보다 더 설치했으니까 간격 넓힘
+            } else {
                 if(ans < mid) ans = mid;
                 min = mid + 1;
-
             }
         }
 
         sb.append(ans).append("\n");
         bw.write(String.valueOf(sb));
-
         bw.flush();
         bw.close();
         br.close();
