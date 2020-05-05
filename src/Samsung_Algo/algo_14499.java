631fe379dc67a8d5fad9a89f408cf34e1d76601c
giri <donggillee94@gmail.com>
2020-05-05 17:12:33 +0900

    Samsung 14499 algorithm start!!

---
+++ src/Samsung_Algo/algo_14499.java
@@ -0,0 +1,16 @@
+package Samsung_Algo;
+import java.io.*;
+import java.util.*;
+
+public class algo_14499 {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        bw.flush();
+        bw.close();
+        br.close();
+        return;
+    }
+}
