2a7c1772f1d0e8675b9fe2018e963fdaf430627f
giri <donggillee94@gmail.com>
2020-05-10 23:15:43 +0900

    BOJ 코딩테스트 대비 모의고사 1번 문제 함수 정의

--- src/algo_18808.java
+++ src/algo_18808.java
@@ -2,15 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class algo_18808 {
+    static int[][] sticker;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
+        sticker = new int[3][4];
+        test();
+        sticker = new int[10][2];
+        test();
 
         bw.flush();
         bw.close();
         br.close();
         return;
+    }
+    public static void test() {
+        System.out.println(sticker.length + " " + sticker[1].length);
     }
     //call sticker attach function
     //sticker attach function
