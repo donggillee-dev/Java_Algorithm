14f7257b477b7c5ca3b1c8052ccefc9797f7b2d5
giri <donggillee94@gmail.com>
2020-05-14 18:06:23 +0900

    Samsung Algorithm algo_17142 solved!!

--- src/Samsung_Algo/algo_17142.java
+++ src/Samsung_Algo/algo_17142.java
@@ -5,13 +5,12 @@ import java.util.*;
 public class algo_17142 {
     static int N, M, VirusCnt;
     static int[][] VirusLoc;
-//    static char[][] Lab;
-    static int[][] Lab;
+    static char[][] Lab;
     static Stack<SpreadInfo> S = new Stack<>();
     static int ans = Integer.MAX_VALUE;
     static int[] dir_x = {1, 0, -1, 0};
     static int[] dir_y = {0, 1, 0, -1};
-
+    static int BlankSpace = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -19,21 +18,25 @@ public class algo_17142 {
         StringTokenizer stk = new StringTokenizer(br.readLine()); //Input N, M;
         N = Integer.parseInt(stk.nextToken());
         M = Integer.parseInt(stk.nextToken());
-        Lab = new int[N][N];
+        Lab = new char[N][N];
         VirusLoc = new int[10][2];
 
         for(int i = 0; i < N; i++) {
             stk = new StringTokenizer(br.readLine());
             for(int j = 0; j < N; j++) {
-                Lab[i][j] = Integer.parseInt(stk.nextToken());
-                if(Lab[i][j] == 2) {
+                Lab[i][j] = stk.nextToken().charAt(0);
+                if(Lab[i][j] == '2') {
+                    Lab[i][j] = '*';
                     VirusLoc[VirusCnt][0] = i;
                     VirusLoc[VirusCnt][1] = j;
                     VirusCnt++;
                 }
+                if(Lab[i][j] != '1')
+                    BlankSpace++;
             }
         }
         PutVirus(0, 0);
+        if(ans == Integer.MAX_VALUE) ans = -1;
         sb.append(ans).append("\n");
         bw.write(String.valueOf(sb));
         bw.flush();
@@ -47,52 +50,50 @@ public class algo_17142 {
             BFS();
         } else {
             for(int i = start; i < VirusCnt; i++) {
-                Lab[VirusLoc[i][0]][VirusLoc[i][1]] = -1;
+                Lab[VirusLoc[i][0]][VirusLoc[i][1]] = '0';
                 S.push(new SpreadInfo(VirusLoc[i][0], VirusLoc[i][1]));
                 PutVirus(i + 1, cnt + 1);
-                Lab[VirusLoc[i][0]][VirusLoc[i][1]] = 2;
+                Lab[VirusLoc[i][0]][VirusLoc[i][1]] = '*';
                 S.pop();
             }
         }
     }
 
     private static void BFS() {
+        int maxTime = 0;
         int cnt = 0;
-//        boolean[][] Visited = new boolean[N][N];
-        int[][] CopyLab = new int[N][N];
+        int[][] Visited = new int[N][N];
+
         Stack<SpreadInfo> Stack = new Stack<>();
         Stack.addAll(S);
         Queue<VirusInfo> Queue = new LinkedList<>();
 
-        for(int i = 0; i < N; i++) {
-            System.arraycopy(Lab[i], 0, CopyLab[i], 0, N);
-        }
         for(int i = 0; i < M; i++) {
             SpreadInfo tmp = Stack.pop();
-//            Visited[tmp.x][tmp.y] = true;
+            cnt++;
+            Visited[tmp.x][tmp.y] = -1;
             Queue.offer(new VirusInfo(tmp.x, tmp.y, 0));
         }
         while(!Queue.isEmpty()) {
             VirusInfo tmp = Queue.poll();
-//            if(tmp.t > ans) break;
+            int Time = tmp.t + 1;
             for(int i = 0; i < 4; i++) {
-                int Time = tmp.t + 1;
                 int nx = tmp.x + dir_x[i];
                 int ny = tmp.y + dir_y[i];
                 if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-//                if(CopyLab[nx][ny] != 1 && !Visited[nx][ny]) {
-//                    CopyLab[nx][ny] = Time;
-//                    Visited[nx][ny] = true;
-//                    Queue.offer(new VirusInfo(nx, ny, Time));
-//                    if(cnt < Time) cnt = Time;
-//                }
+                if(Lab[nx][ny] != '1' && Visited[nx][ny] == 0) {
+                    cnt++;
+                    if(Lab[nx][ny] == '*') {
+                        Visited[nx][ny] = -1;
+                    } else {
+                        Visited[nx][ny] = Time;
+                        if(maxTime < Time) maxTime = Time;
+                    }
+                    Queue.offer(new VirusInfo(nx, ny, Time));
+                }
             }
         }
-        for(int i = 0; i < N; i++) {
-            System.out.println(Arrays.toString(CopyLab[i]));
-        }
-        System.out.println();
-        if(cnt < ans) ans = cnt;
+        if(maxTime < ans && cnt == BlankSpace) ans = maxTime;
     }
 }
 class SpreadInfo {
