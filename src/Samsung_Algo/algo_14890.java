0f2c8685e7534f9e179a6aa454b668e863ba52cd
giri <donggillee94@gmail.com>
2020-05-14 21:24:32 +0900

    Samsung Algorithm algo_14890 made function architect

--- src/Samsung_Algo/algo_14890.java
+++ src/Samsung_Algo/algo_14890.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class algo_14890 {
     static int N, L, ans = 0;
     static int[][] Map;
+    static boolean[][] Built;
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -13,46 +14,131 @@ public class algo_14890 {
         N = Integer.parseInt(stk.nextToken());
         L = Integer.parseInt(stk.nextToken());
         Map = new int[N][N];
+        Built = new boolean[N][N];
         for(int i = 0; i < N; i++) {
             stk = new StringTokenizer(br.readLine());
             for(int j = 0; j < N; j++) {
                 Map[i][j] = Integer.parseInt(stk.nextToken());
             }
         }
-        for(int i = 0; i < N; i++) {//행에 대해서
-            int UpStair = 0;//올라가는 경사로 설치?
-            int DownStair = 0;//내려가는 경사로 설치
-            int start_idx = 0;//기준점 되는 idx
-            for(int j = 1; j < N; j++) {
-                if(Map[i][start_idx] != Map[i][j]) {
-                    int diff = Map[i][start_idx] - Map[i][j];
-
-                    if(diff == -1) {
-                        if(DownStair != 0) {
-
-                        }
-                        DownStair++;
-                    } else if(diff == 1) {
-
-                    } else break;
-
-                    start_idx = j;
-                }
-            }
+        for(int i = 0; i < N; i++) {
+            RowPossible(i);
         }
-
-        for(int i = 0; i < N; i++) {//열에 대해서
-            for(int j = 0; j < N - 1; j++) {//위 -> 아래
-
-            }
-
-            for(int j = N - 1; j > 0; j--) {//아래 -> 위
-
-            }
+        for(int i = 0; i < N; i++) {
+            ColumnPossible(i);
         }
+        sb.append(ans).append("\n");
+        bw.write(String.valueOf(sb));
         bw.flush();
         bw.close();
         br.close();
         return;
+    }
+    private static void RowPossible(int Row) {
+        boolean Possible = true;
+        int length = 1;
+        int height = Map[Row][0];
+
+        loop1:for(int j = 1; j < N; j++) {
+            if(Math.abs(Map[Row][j] - height) > 1) {
+                Possible = false;
+                break loop1;
+            }
+
+            if(Map[Row][j] > height) {//올라가는 경사
+                if(length < L) {
+                    Possible = false;
+                    break loop1;
+                }
+                for(int k = 1, idx = j - k; k <= L && idx >= 0; k++) {
+                    if(Built[Row][idx]) {
+                        Possible = false;
+                        break loop1;
+                    }
+                }
+                for(int k = 1, idx = j - k; k <= L && idx >= 0; k++) {
+                    Built[Row][idx] = true;
+                }
+                height = Map[Row][j];
+                length = 1;
+            } else if(Map[Row][j] < height) {//내려가는 경사
+                height = Map[Row][j];
+                for(int k = 1, idx = j + k; k < L && idx < N; k++) {
+                    if(height != Map[Row][idx] || Built[Row][idx]) {
+                        Possible = false;
+                        break loop1;
+                    }
+                }
+                for(int k = 0, idx = j + k; idx < N && k < L; k++) {
+                    Built[Row][idx] = true;
+                }
+                j += (L - 1);
+                height = Map[Row][j];
+                length = 1;
+            } else {//똑같은 높이인 경우
+                length++;
+            }
+        }
+        if(Possible) {
+            System.out.println(Row);
+            ans++;
+        }
+        Init_Built();
+    }
+    private static void ColumnPossible(int Column) {
+        boolean Possible = true;
+        int length = 1;
+        int height = Map[0][Column];
+
+        loop1:for(int j = 1; j < N; j++) {
+            if(Math.abs(Map[j][Column] - height) > 1) {
+                Possible = false;
+                break loop1;
+            }
+
+            if(Map[j][Column] > height) {//올라가는 경사
+                if(length < L) {
+                    Possible = false;
+                    break loop1;
+                }
+                for(int k = 1, idx = j - k; k <= L && idx >= 0; k++) {
+                    if(Built[idx][Column]) {
+                        Possible = false;
+                        break loop1;
+                    }
+                }
+                for(int k = 1, idx = j - k; k <= L && idx >= 0; k++) {
+                    Built[idx][Column] = true;
+                }
+                height = Map[j][Column];
+                length = 1;
+            } else if(Map[j][Column] < height) {//내려가는 경사 -> 다시 생각해볼 것
+                height = Map[j][Column];
+                for(int k = 1, idx = j + k; k < L && idx < N; k++) {
+                    if(height != Map[idx][Column] || Built[idx][Column]) {
+                        Possible = false;
+                        break loop1;
+                    }
+                }
+                for(int k = 0, idx = j + k; idx < N && k < L; k++) {
+                    Built[idx][Column] = true;
+                }
+                j += (L - 1);
+                height = Map[j][Column];
+                length = 1;
+            } else {//똑같은 높이인 경우
+                length++;
+            }
+        }
+        if(Possible) {
+            System.out.println(Column);
+            ans++;
+        }
+        Init_Built();
+    }
+    private static void Init_Built() {
+        for(int i = 0; i < N; i++) {
+            Arrays.fill(Built[i], false);
+        }
     }
 }
