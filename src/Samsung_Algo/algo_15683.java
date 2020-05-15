f0f8b7edfb5daa779aa45731d145d6b2f53e71e9
giri <donggillee94@gmail.com>
2020-05-15 18:47:14 +0900

    Samsung Algorithm algo_15683 solved!!

---
+++ src/Samsung_Algo/algo_15683.java
@@ -0,0 +1,16 @@
+package Samsung_Algo;
+import java.io.*;
+import java.util.*;
+
+public class algo_15683 {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        bw.flush();
+        bw.close();
+        br.close();
+        return;
+    }
+}
