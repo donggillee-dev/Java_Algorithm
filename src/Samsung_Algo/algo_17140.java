dc82beb3cc4c11e245387172f83fec74c547b4a6
giri <donggillee94@gmail.com>
2020-05-21 15:16:14 +0900

    Samsung Algorithm algo_17140 solved!!

--- src/Samsung_Algo/algo_17140.java
+++ src/Samsung_Algo/algo_17140.java
@@ -1,22 +1,38 @@
 package Samsung_Algo;
 import java.io.*;
 import java.util.*;
+
 public class algo_17140 {
-    private static class Info {
-        int x, y;
-        int val;
-        public Info(int nx, int ny, int v) {
-            this.x = nx;
-            this.y = ny;
+    static private class Info {
+        int val, frq;
+        public Info(int v, int f) {
             this.val = v;
+            this.frq = f;
+        }
+    }
+    static private class Compare implements Comparator<Info> {
+        @Override
+        public int compare(Info o1, Info o2) {
+            if(o1.frq > o2.frq) {
+                return 1;
+            } else if(o1.frq == o2.frq) {
+                if(o1.val > o2.val) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            } else {
+                return -1;
+            }
         }
     }
     static int Row = 3, Column = 3;
     static int R, C, K;
     static int ans = 0;
+    static int[][] arr = new int[101][101];
+    static int[] cnt_arr = new int[300];
     static LinkedList<Info> List = new LinkedList<>();
-
-    public static void main(String[] args[]) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
@@ -28,11 +44,16 @@ public class algo_17140 {
             stk = new StringTokenizer(br.readLine());
             for(int j = 1; j <= 3; j++) {
                 int val = Integer.parseInt(stk.nextToken());
-                if(val == K && i == R && j == K) {
-                    break loop1;
-                }
-                List.add(new Info(i, j, val));
+                arr[i][j] = val;
             }
+        }
+        while(ans <= 100) {
+            CntRow();
+            CntCol();
+            if(arr[R][C] == K) break;
+            if(Row >= Column) R_cal();
+            else if(Row < Column)C_cal();
+            ans++;
         }
         if(ans > 100) ans = -1;
         sb.append(ans).append("\n");
@@ -42,9 +63,84 @@ public class algo_17140 {
         br.close();
         return;
     }
-    private static void solve() {
-        Collections.sort(List, Comparator.comparingInt(o -> o.x));//행에 대해 정렬
-        Collections.sort(List, Comparator.comparingInt(o -> o.y));//열에 대해 정렬
-        Collections.sort(List, Comparator.comparingInt(o -> o.val));//값에 대해 정렬
+    private static void R_cal() {
+        for(int i = 1; i <= 100; i++) {
+            for(int j = 1; j <= 100; j++) {
+                if(arr[i][j] != 0) {
+                    cnt_arr[arr[i][j]]++;
+                }
+            }
+            for(int j = 1; j < 300; j++) {
+                if(cnt_arr[j] != 0) {
+                    List.add(new Info(j, cnt_arr[j]));
+                }
+            }
+            Collections.sort(List, new Compare());
+            Info tmp;
+            Arrays.fill(arr[i], 0);
+            int k = 1;
+            for(Iterator<Info> it = List.iterator(); it.hasNext();) {
+                tmp = it.next();
+                arr[i][k] = tmp.val;
+                arr[i][k + 1] = tmp.frq;
+                k += 2;
+                it.remove();
+            }
+            Arrays.fill(cnt_arr, 0);
+        }
+    }
+    private static void C_cal() {
+        for(int i = 1; i <= 100; i++) {
+            for(int j = 1; j <= 100; j++) {
+                if(arr[j][i] != 0) {
+                    cnt_arr[arr[j][i]]++;
+                }
+            }
+            for(int j = 1; j < 300; j++) {
+                if(cnt_arr[j] != 0) {
+                    List.add(new Info(j, cnt_arr[j]));
+                }
+            }
+            Collections.sort(List, new Compare());
+            for(int j = 1; j <= Row; j++) {
+                arr[j][i] = 0;
+            }
+            Info tmp;
+            int k = 1;
+            for(Iterator<Info> it = List.iterator(); it.hasNext();) {
+                tmp = it.next();
+                arr[k][i] = tmp.val;
+                arr[k + 1][i] = tmp.frq;
+                k += 2;
+                it.remove();
+            }
+            Arrays.fill(cnt_arr, 0);
+        }
+    }
+    private static void CntRow() {
+        int cnt = 0;
+        for(int i = 1; i <= 100; i++) {
+            int k = 0;
+            for(int j = 1; j  <= 100; j++) {
+                if(arr[j][i] != 0) {
+                    k++;
+                }
+            }
+            cnt = Math.max(cnt, k);
+        }
+        Row = Math.max(cnt, Row);
+    }
+    private static void CntCol() {
+        int cnt = 0;
+        for(int i = 1; i <= 100; i++) {
+            int k = 0;
+            for(int j = 1; j  <= 100; j++) {
+                if(arr[i][j] != 0) {
+                    k++;
+                }
+            }
+            cnt = Math.max(cnt, k);
+        }
+        Column = Math.max(cnt, Column);
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,43 @@
 import java.io.*;
-import java.util.Arrays;
+import java.util.*;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        char[][] arr = new char[3][3];
-        arr[0][0] = 'W';
-        arr[0][1] = 'W';
-        arr[0][2] = 'W';
-        sb.append(Arrays.toString(arr[0]));
-        System.out.println(sb);
+        LinkedList<Integer> L = new LinkedList<>();
+        LinkedList<Integer> Child = new LinkedList<>();
+        L.add(1);
+        L.add(2);
+        L.add(2);
+        L.add(2);
+        L.add(4);
+        L.add(4);
+        L.add(5);
+        int val = 0;
+        int cnt = 1;
+        int idx = 1;
+        for(Iterator<Integer> it = L.iterator(); it.hasNext();) {
+            int cur = it.next();
+            it.remove();
+            if(idx == 1) {
+                val = cnt;
+            } else {
+                if(val != cur) {
+                    Child.add(val);
+                    Child.add(cnt);
+                    val = cur;
+                    cnt = 1;
+                } else {
+                    cnt++;
+                }
+            }
+            idx++;
+        }
+        Child.add(val);
+        Child.add(cnt);
+        for(Iterator<Integer> it = Child.iterator(); it.hasNext();) {
+            System.out.println(it.next());
+        }
+        System.out.println(Child.size());
+        System.out.println(L.size());
     }
 }
