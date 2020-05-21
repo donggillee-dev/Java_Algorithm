3f58af592885581e8d1eee8910d214423a1bf957
giri <donggillee94@gmail.com>
2020-05-21 15:19:44 +0900

    Samsung Algorithm algo_17140 add comment!!

--- src/Samsung_Algo/algo_17140.java
+++ src/Samsung_Algo/algo_17140.java
@@ -65,21 +65,21 @@ public class algo_17140 {
     }
     private static void R_cal() {
         for(int i = 1; i <= 100; i++) {
-            for(int j = 1; j <= 100; j++) {
+            for(int j = 1; j <= 100; j++) {//행에 있는 값들에 대해 나타나는 빈도수 체크
                 if(arr[i][j] != 0) {
                     cnt_arr[arr[i][j]]++;
                 }
             }
-            for(int j = 1; j < 300; j++) {
+            for(int j = 1; j < 300; j++) {//해당 값의 빈도수를 List에 넣어줌
                 if(cnt_arr[j] != 0) {
                     List.add(new Info(j, cnt_arr[j]));
                 }
             }
-            Collections.sort(List, new Compare());
+            Collections.sort(List, new Compare());//정렬 기준으로 List 정렬
             Info tmp;
-            Arrays.fill(arr[i], 0);
+            Arrays.fill(arr[i], 0);//기존의 행을 초기화해줌
             int k = 1;
-            for(Iterator<Info> it = List.iterator(); it.hasNext();) {
+            for(Iterator<Info> it = List.iterator(); it.hasNext();) {//배열에 값 , 빈도수 이렇게 채워넣어줌
                 tmp = it.next();
                 arr[i][k] = tmp.val;
                 arr[i][k + 1] = tmp.frq;
@@ -91,23 +91,23 @@ public class algo_17140 {
     }
     private static void C_cal() {
         for(int i = 1; i <= 100; i++) {
-            for(int j = 1; j <= 100; j++) {
+            for(int j = 1; j <= 100; j++) {//열에 대해 값, 빈도수 체크 위한 cnt_arr 구성
                 if(arr[j][i] != 0) {
                     cnt_arr[arr[j][i]]++;
                 }
             }
             for(int j = 1; j < 300; j++) {
-                if(cnt_arr[j] != 0) {
+                if(cnt_arr[j] != 0) {//cnt_arr을 통해 List에 값, 빈도수 넣어줌
                     List.add(new Info(j, cnt_arr[j]));
                 }
             }
-            Collections.sort(List, new Compare());
-            for(int j = 1; j <= Row; j++) {
+            Collections.sort(List, new Compare());//정렬 규칙대로 정렬
+            for(int j = 1; j <= Row; j++) {//기존의 열을 초기화 -> 100의 for문을 돌면 시간초과의 위험 -> 최대 행(값이 존재하는 최대 행 idx)만틈
                 arr[j][i] = 0;
             }
             Info tmp;
             int k = 1;
-            for(Iterator<Info> it = List.iterator(); it.hasNext();) {
+            for(Iterator<Info> it = List.iterator(); it.hasNext();) {//열에 대해 다시 값 세팅
                 tmp = it.next();
                 arr[k][i] = tmp.val;
                 arr[k + 1][i] = tmp.frq;
