9a3f7fe740160325e20ad3cdd8903bcba6310494
giri <donggillee94@gmail.com>
2020-05-23 19:14:56 +0900

    Samsung Algorithm algo_17144 solved!!

--- src/Samsung_Algo/algo_17144.java
+++ src/Samsung_Algo/algo_17144.java
@@ -3,37 +3,11 @@ import java.io.*;
 import java.util.*;
 
 public class algo_17144 {
-    private static class Info {
-        int x, y;
-        public Info(int nx, int ny) {
-            this.x = nx;
-            this.y = ny;
-        }
-    }
-    private static class Info2 {
-        int x, y, val;
-        boolean isFirst;
-        public Info2(int nx, int ny, int v, boolean f) {
-            this.x = nx;
-            this.y = ny;
-            this.val = v;
-            this.isFirst = f;
-        }
-    }
-    private static class Info3 {
-        int x, y, val;
-        public Info3(int nx, int ny, int v) {
-            this.x = nx;
-            this.y = ny;
-            this.val = v;
-        }
-    }
     static int R, C, T, ans = 0;
     static int UpperX, UpperY, DownX, DownY;
     static int[][] Arr;
     static int[] dir_x = {0, 0, -1, 1};
     static int[] dir_y = {-1, 1, 0, 0};
-    static LinkedList<Info> List = new LinkedList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -57,9 +31,7 @@ public class algo_17144 {
                 } else if(val == -1 && isUpper) {
                     DownX = i;
                     DownY = j;
-                } else if(val > 0) {
-                    List.add(new Info(i, j));
-                }
+                } else;
                 Arr[i][j] = val;
             }
         }
@@ -78,120 +50,73 @@ public class algo_17144 {
     }
 
     private static void Spread() {
-        Info tmp;
-        Info2 tmp2;
-        LinkedList<Info2> Sub = new LinkedList<>();
-        for(int idx = 0; idx < List.size(); idx++) {
-            tmp = List.get(idx);
-            int Amount = Arr[tmp.x][tmp.y] / 5;
-            int cnt = 0;
-            for(int i = 0; i < 4; i++) {
-                int nx = tmp.x + dir_x[i];
-                int ny = tmp.y + dir_y[i];
-                if(nx < 1 || ny < 1 || nx > R || ny > C) continue;
-                if((nx == UpperX && ny == UpperY) || (nx == DownX && ny == DownY)) continue;
-                if(Arr[nx][ny] == 0 && Amount > 0) {
-                    Sub.add(new Info2(nx, ny, Amount, true));
-                } else if(Arr[nx][ny] != 0 && Amount > 0) {
-                    Sub.add(new Info2(nx, ny, Amount, false));
+        int[][] sum = new int[R + 1][C + 1];
+        for(int i = 1; i <= R; i++) {
+            for(int j = 1; j <= C; j++) {
+                int cnt = 0;
+                int val = 0;
+                if(Arr[i][j] > 4) {
+                    int nx;
+                    int ny;
+                    val = Arr[i][j] / 5;
+                    for(int k = 0; k < 4; k++) {
+                        nx = i + dir_x[k];
+                        ny = j + dir_y[k];
+                        if(nx < 1 || ny < 1 || nx > R || ny > C || (nx == UpperX && ny == UpperY) || (nx == DownX && ny == DownY)) continue;
+                        sum[nx][ny] += val;
+                        cnt++;
+                    }
+                    Arr[i][j] -= (val * cnt);
                 }
-                cnt++;
             }
-            Arr[tmp.x][tmp.y] -= (Amount * cnt);
         }
-        for(int i = 0; i < Sub.size(); i++) {
-            tmp2 = Sub.get(i);
-            if(tmp2.isFirst && Arr[tmp2.x][tmp2.y] == 0) {
-                List.add(new Info(tmp2.x, tmp2.y));
+        for(int i = 1; i <= R; i++) {
+            for(int j = 1; j <= C; j++) {
+                if(sum[i][j] != 0) {
+                    Arr[i][j] += sum[i][j];
+                }
             }
-            Arr[tmp2.x][tmp2.y] += tmp2.val;
         }
     }
     private static void Move() {
-        LinkedList<Info> Rm = new LinkedList<>();
-        LinkedList<Info3> Sub = new LinkedList<>();
-        Info tmp;
-        int val;
-        for(int i = 0; i < List.size(); i++) {
-            tmp = List.get(i);
-            if(tmp.y == 1 && tmp.x < UpperX) {
-                if(tmp.x == UpperX - 1) {
-                    Rm.add(tmp);
-                    List.remove(i);
-                    i--;
-                } else {
-                    val = Arr[tmp.x][tmp.y];
-                    Arr[tmp.x][tmp.y] = 0;
-                    tmp.x++;
-                    List.set(i, tmp);
-                    Sub.add(new Info3(tmp.x, tmp.y, val));
-                }
-            } else if(tmp.y == 1 && tmp.x > DownX) {
-                if(tmp.x == DownX + 1) {
-                    Rm.add(tmp);
-                    List.remove(i);
-                    i--;
-                } else {
-                    val = Arr[tmp.x][tmp.y];
-                    Arr[tmp.x][tmp.y] = 0;
-                    tmp.x--;
-                    List.set(i, tmp);
-                    Sub.add(new Info3(tmp.x, tmp.y, val));
-                }
-            } else if(tmp.x == 1 && tmp.y >= 2) {
-                val = Arr[tmp.x][tmp.y];
-                Arr[tmp.x][tmp.y] = 0;
-                tmp.y--;
-                List.set(i, tmp);
-                Sub.add(new Info3(tmp.x, tmp.y, val));
-            } else if(tmp.x == R && tmp.y >= 2) {
-                val = Arr[tmp.x][tmp.y];
-                Arr[tmp.x][tmp.y] = 0;
-                tmp.y--;
-                List.set(i, tmp);
-                Sub.add(new Info3(tmp.x, tmp.y, val));
-            } else if(tmp.x == UpperX && tmp.y <= C - 1) {
-                val = Arr[tmp.x][tmp.y];
-                Arr[tmp.x][tmp.y] = 0;
-                tmp.y++;
-                List.set(i, tmp);
-                Sub.add(new Info3(tmp.x, tmp.y, val));
-            } else if(tmp.x == DownX && tmp.y <= C - 1){
-                val = Arr[tmp.x][tmp.y];
-                Arr[tmp.x][tmp.y] = 0;
-                tmp.y++;
-                List.set(i, tmp);
-                Sub.add(new Info3(tmp.x, tmp.y, val));
-            } else if(tmp.y == C && tmp.x >= DownX && tmp.x <= R - 1) {
-                val = Arr[tmp.x][tmp.y];
-                Arr[tmp.x][tmp.y] = 0;
-                tmp.x++;
-                List.set(i, tmp);
-                Sub.add(new Info3(tmp.x, tmp.y, val));
-            } else if(tmp.y == C && tmp.x <= UpperX && tmp.x >= 1) {
-                val = Arr[tmp.x][tmp.y];
-                Arr[tmp.x][tmp.y] = 0;
-                tmp.x--;
-                List.set(i, tmp);
-                Sub.add(new Info3(tmp.x, tmp.y, val));
-            } else;
+        Arr[UpperX - 1][UpperY] = 0;
+        for(int i = UpperX - 1; i >= 2; i--) {
+            Arr[i][1] = Arr[i - 1][1];
         }
-        //Upper
-        for(int i = 0; i < Rm.size(); i++) {
-            tmp = Rm.get(i);
-            Arr[tmp.x][tmp.y] = 0;
+        for(int i = 1; i <= C - 1; i++) {
+            Arr[1][i] = Arr[1][i + 1];
         }
-        Info3 tmp2;
-        for(int i = 0; i < Sub.size(); i++) {
-            tmp2 = Sub.get(i);
-            Arr[tmp2.x][tmp2.y] = tmp2.val;
+        for(int i = 1; i <= UpperX - 1; i++) {
+            Arr[i][C] = Arr[i + 1][C];
         }
+        for(int i = C; i > UpperY + 1; i--) {
+            Arr[UpperX][i] = Arr[UpperX][i - 1];
+        }
+        Arr[UpperX][UpperY + 1] = 0;
+
+        Arr[DownX + 1][DownY] = 0;
+        for(int i = DownX + 1; i <= R - 1; i++) {
+            Arr[i][1] = Arr[i + 1][1];
+        }
+        for(int i = 1; i <= C - 1; i++) {
+            Arr[R][i] = Arr[R][i + 1];
+        }
+        for(int i = R; i >= DownX + 1; i--) {
+            Arr[i][C] = Arr[i - 1][C];
+        }
+        for(int i = C; i > DownY + 1; i--) {
+            Arr[DownX][i] = Arr[DownX][i - 1];
+        }
+        Arr[DownX][DownY + 1] = 0;
+
     }
     private static void solve() {
-        Info tmp;
-        for(int i = 0; i < List.size(); i++) {
-            tmp = List.get(i);
-            ans += Arr[tmp.x][tmp.y];
+        for(int i = 1; i <= R; i++) {
+            for(int j = 1; j <= C; j++) {
+                if(Arr[i][j] > 0) {
+                    ans += Arr[i][j];
+                }
+            }
         }
     }
 }
