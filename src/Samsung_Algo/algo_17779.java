186041b8abf42f5589ea79d21bcbdfc09b732728
giri <donggillee94@gmail.com>
2020-05-24 21:07:54 +0900

    Samsung Algorithm algo_17779 start!!

---
+++ src/Samsung_Algo/algo_17779.java
@@ -0,0 +1,16 @@
+package Samsung_Algo;
+import java.io.*;
+import java.util.*;
+
+public class algo_17779 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        bw.flush();
+        bw.close();
+        br.close();
+        return;
+    }
+}
