9f32455b2d16582d857912502c6f536d453c469e
giri <donggillee94@gmail.com>
2020-05-28 21:14:49 +0900

    BOJ Step by Step 1006 solving...!!

--- src/BOJ_StepByStep/algo_1006.java
+++ src/BOJ_StepByStep/algo_1006.java
@@ -1,4 +1,16 @@
 package BOJ_StepByStep;
+import java.io.*;
+import java.util.*;
 
 public class algo_1006 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        bw.flush();
+        bw.close();
+        br.close();
+        return;
+    }
 }
