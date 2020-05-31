96e55aeb401188ec87998cc21f6e438ac02860ef
giri <donggillee94@gmail.com>
2020-05-31 19:04:12 +0900

    BOJ Step by Step algo_1010 start!!

---
+++ src/BOJ_StepByStep/algo_1010.java
@@ -0,0 +1,37 @@
+package BOJ_StepByStep;
+import java.io.*;
+import java.util.*;
+
+public class algo_1010 {
+    static long ans = 1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer stk;
+        for(int Case = 0; Case < T; Case++) {
+            stk = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(stk.nextToken());
+            int M = Integer.parseInt(stk.nextToken());
+            if(N != M) {
+                Solve(1, N, 0, M);
+            }
+            sb.append(ans).append("\n");
+            ans = 1;
+        }
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+        br.close();
+        return;
+    }
+    private static void Solve(int depth, int N, int start, int M) {
+        int end = M - start - (N - depth);
+        if(depth == N) {
+            return;
+        } else {
+            Solve(depth + 1, N, start + 1, M);
+        }
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -3,41 +3,13 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        LinkedList<Integer> L = new LinkedList<>();
-        LinkedList<Integer> Child = new LinkedList<>();
-        L.add(1);
-        L.add(2);
-        L.add(2);
-        L.add(2);
-        L.add(4);
-        L.add(4);
-        L.add(5);
-        int val = 0;
-        int cnt = 1;
-        int idx = 1;
-        for(Iterator<Integer> it = L.iterator(); it.hasNext();) {
-            int cur = it.next();
-            it.remove();
-            if(idx == 1) {
-                val = cnt;
-            } else {
-                if(val != cur) {
-                    Child.add(val);
-                    Child.add(cnt);
-                    val = cur;
-                    cnt = 1;
-                } else {
-                    cnt++;
-                }
-            }
-            idx++;
+        int ans = 1;
+        int input = 17;
+        while(input >= 1) {
+            ans *= input;
+            System.out.println(ans);
+            input--;
         }
-        Child.add(val);
-        Child.add(cnt);
-        for(Iterator<Integer> it = Child.iterator(); it.hasNext();) {
-            System.out.println(it.next());
-        }
-        System.out.println(Child.size());
-        System.out.println(L.size());
+
     }
 }
