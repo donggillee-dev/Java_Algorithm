3ee6a7ddbb97e937859835287cbb601ac7fa68b0
giri <donggillee94@gmail.com>
2020-10-22 16:39:34 +0900

    Programmers 고득점 Kit Heap 더 맵게 Problem
    Solved!!

---
+++ src/Programmers_Kit_Heap/MoreSpicy.java
@@ -0,0 +1,34 @@
+package Programmers_Kit_Heap;
+import java.util.*;
+
+public class MoreSpicy {
+    public static void main(String[] args) {
+        int[] scoville = {1, 2, 3, 9, 10, 12};
+        int K = 7;
+
+        Solution sol = new Solution();
+
+        System.out.println(sol.solution(scoville, K));
+    }
+    private static class Solution {
+        public int solution(int[] scoville, int K) {
+            int answer = 0;
+            PriorityQueue<Integer> Q = new PriorityQueue<>();
+
+            for(int i = 0; i < scoville.length; i++) { //우선순위 큐에 넣어줌
+                Q.offer(scoville[i]);
+            }
+
+            while(Q.size() > 1 && Q.peek() < K) {
+                int first = Q.poll();
+                int second = Q.poll();
+                int mix = (first + second*2);
+                Q.offer(mix);
+                answer++;
+            }
+
+            if(Q.size() <= 1 && Q.peek() < K) answer = -1;
+            return answer;
+        }
+    }
+}
