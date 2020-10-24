bb7c80b6ef1ae7a7a4ae52dfcaa0acd656951ce8
giri <donggillee94@gmail.com>
2020-10-24 18:26:06 +0900

    NHN Coding Test number1 solved number3 solved

--- src/NHN_PreTest/Number3.java
+++ src/NHN_PreTest/Number3.java
@@ -12,36 +12,63 @@ public class Number3 {
     }
     private static void solution(int numOfOrder, String[] orderArr) throws IOException {
         Stack<Character> S = new Stack<>();
-        Queue<String> answerQueue = new LinkedList<>();
+        Stack<Character> answerStack = new Stack<>();
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
         for(int i = 0; i < numOfOrder; i++) {
             String order = orderArr[i];
             String innerStr = "";
+            String answer = "";
             for(int j = 0; j < order.length(); j++) {
                 char order_ch = order.charAt(j);
-                boolean isInner = false;
-                if(order_ch == ')') {
+                if (order_ch == ')') {
                     char pop_ch;
-                    while(true) {
+                    while (true) {
                         pop_ch = S.pop();
-                        if(pop_ch == '(') {
-                            isInner = true;
-                            break;
-                        } else if(Character.isDigit(pop_ch)){
-                            break;
-                        } else innerStr = pop_ch + innerStr;
+                        if (pop_ch == '(') break;
+                        else innerStr = pop_ch + innerStr;
                     }
-                    if(isInner) {
-
+                    pop_ch = S.pop();
+                    if (Character.isDigit(pop_ch)) {
+                        String tmp = "";
+                        for (int idx = 0; idx < pop_ch - '0'; idx++) {
+                            tmp += innerStr;
+                        }
+                        for (int idx = 0; idx < tmp.length(); idx++) {
+                            S.push(tmp.charAt(idx));
+                        }
                     } else {
-
+                        String tmp = "";
+                        for (int idx = 0; idx < innerStr.length(); idx++) {
+                            tmp += pop_ch;
+                            tmp += innerStr.charAt(idx);
+                        }
+                        for (int idx = 0; idx < tmp.length(); idx++) {
+                            S.push(tmp.charAt(idx));
+                        }
                     }
                 } else {
                     S.push(order_ch);
                 }
+                innerStr = "";
             }
+            while(!S.isEmpty()) {
+                answerStack.add(S.pop());
+            }
+
+            while(!answerStack.isEmpty()) {
+                char ch_pop = answerStack.pop();
+
+                if(Character.isDigit(ch_pop)) {
+                    char ch = answerStack.pop();
+                    for(int idx = 0; idx < ch_pop - '0'; idx++) {
+                        answer += ch;
+                    }
+                } else answer += ch_pop;
+
+            }
+            System.out.println(answer);
         }
         bw.write(String.valueOf(sb));
         bw.flush();
