51ef05fc3cb344d25ec7eefb7ae6a10f1d9cd84a
giri <donggillee94@gmail.com>
2020-11-06 17:24:34 +0900

    Programmers Level2 KakaoFriends ColoringBook problem solved

--- src/Programmers_Lv3/N2_Tiling.java
+++ src/Programmers_Lv3/N2_Tiling.java
@@ -1,16 +1,15 @@
 package Programmers_Lv3;
 
 public class N2_Tiling {
+
     public static void main(String[] args) {
         int n = 60000;
         Solution sol = new Solution();
-
         System.out.println(sol.solution(n));
     }
     private static class Solution {
         public int solution(int n) {
             int answer = 0;
-
             if(n == 1) return 1;
             else if(n == 2) return 2;
 
