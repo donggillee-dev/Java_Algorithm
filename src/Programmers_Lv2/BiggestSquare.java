00b981101633d8d14697da392b0988d224737aec
giri <donggillee94@gmail.com>
2020-11-09 17:09:25 +0900

    Programmers Level2 Biggest Square problem solved!!

--- src/Programmers_Lv2/BiggestSquare.java
+++ src/Programmers_Lv2/BiggestSquare.java
@@ -2,66 +2,31 @@ package Programmers_Lv2;
 
 public class BiggestSquare {
     public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[][] board = {{0,1,1,1},{1,1,1,1},{1,1,1,1},{0,0,1,0}};
+        int answer = sol.solution(board);
 
+        System.out.println(answer);
+
+        return;
     }
     private static class Solution {
-        static int answer = Integer.MIN_VALUE;
-        // static boolean[][] visited;
         public int solution(int [][]board) {
-            // visited = new boolean[board.length][board[0].length];
+            int answer = Integer.MIN_VALUE;
 
-            for(int i = 0; i < board.length; i++) {
+            for(int i = 0; i < board[0].length; i++) {
+                if(board[0][i] == 1) answer = 1;
+            }
+            for(int i = 1; i < board.length; i++) {
                 for(int j = 0; j < board[i].length; j++) {
-                    if(board[i][j] == 1) {
-                        int length = getLength(i, j, board);
-                        if(length != -1) {
-                            // changeVisit(i, j, length);
-                            answer = Math.max(answer, length * length);
-                        }
+                    if(j - 1 < 0) continue;
+                    if(board[i][j] != 0) {
+                        board[i][j] = Math.min(board[i][j - 1], Math.min(board[i-1][j], board[i - 1][j - 1])) + 1;
+                        answer = answer < board[i][j] ? board[i][j] : answer;
                     }
                 }
             }
-            return answer;
-        }
-//     public void changeVisit(int x, int y, int length) {
-//         for(int i = x; i < x + length; i++) {
-//             for(int j = y; j < y + length; j++) {
-//                 visited[i][j] = true;
-//             }
-//         }
-
-        //         return;
-//     }
-        public int getLength(int x, int y, int[][] board) {
-            int lengthX = 0;
-            int lengthY = 0;
-
-            boolean isFullWith = true;
-
-            for(int i = x; i < board.length; i++) {
-                if(board[i][y] != 1) break;
-                lengthX++;
-            }
-            for(int i = y; i < board[x].length; i++) {
-                if(board[x][i] != 1) break;
-                lengthY++;
-            }
-
-            if(lengthX == lengthY && lengthX != 0 && lengthY != 0) {
-                loop:for(int i = x; i < x + lengthX; i++) {
-                    for(int j = y; j < y + lengthY; j++) {
-                        if(board[i][j] != 1) {
-                            isFullWith = false;
-                            break loop;
-                        }
-                    }
-                }
-                if(isFullWith)
-                    return lengthX;
-                else return -1;
-            } else {
-                return -1;
-            }
+            return answer * answer;
         }
     }
 }
