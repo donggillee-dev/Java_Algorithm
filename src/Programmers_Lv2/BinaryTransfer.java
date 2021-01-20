fa09ed47b05aff30204c7bad6d2556b5791eaaf1
giri <donggillee94@gmail.com>
2021-01-20 17:48:54 +0900

    Programmers Lv2 이진 변환 반복하기 Solved!!
    시뮬레이션만 하면 되는 문제이다.
    
    자바에서 String 객체가 immutable하기 때문에 메모리와 시간 복잡도를 위해 StringBuilder를 통해 문자열을 조작해주었다.

---
+++ src/Programmers_Lv2/BinaryTransfer.java
@@ -0,0 +1,42 @@
+package Programmers_Lv2;
+
+import java.io.*;
+
+public class BinaryTransfer {
+    public static void main(String[] args) throws IOException {
+        //** 로직 두개를 구현하면 된다
+        //1. x의 모든 0을 제거하는 로직
+        //2. x의 길이를 c라 했을시 c 를 이진법으로 변환하는 로직
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int[] answer = new int[2];
+
+        sb.append(br.readLine());
+        //x의 모든 0을 제거하는 로직
+        while(sb.length() > 1) {
+            for(int i = 0; i < sb.length(); i++) {
+                if(sb.charAt(i) == '0') {
+                    sb.delete(i, i + 1);
+                    i--;
+                    answer[1]++;
+                }
+            }
+
+            int num = sb.length();
+            sb.delete(0, sb.length());
+            while(num > 1) {
+                sb.insert(0, num % 2);
+                num /= 2;
+            }
+            if(num == 1) sb.insert(0, 1);
+            answer[0]++;
+        }
+        sb.delete(0, sb.length());
+        sb.append(answer[0] + " " + answer[1]).append("\n");
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,15 @@
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.io.*;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int num = 129392;
+        while(num > 1) {
+            sb.insert(0, num % 2);
+            num /= 2;
+        }
+        if(num == 1) sb.insert(0, 1);
 
+        System.out.println(String.valueOf(sb));
     }
 }
