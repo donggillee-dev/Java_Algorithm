f9f7fee804ed72f2102c071048639f2f59fe396c
donggillee-dev <donggillee94@gmail.com>
2021-01-29 21:55:07 +0900

    Programmers Lv2 메뉴 리뉴얼
    문제 풀기 start!!

---
+++ src/Programmers_Lv2/MenuRenewal.java
@@ -0,0 +1,13 @@
+package Programmers_Lv2;
+
+public class MenuRenewal {
+    public static void main(String[] args) {
+
+    }
+    private static class Solution {
+        public String[] solution(String[] orders, int[] course) {
+            String[] answer = {};
+            return answer;
+        }
+    }
+}
