f901fecece278ab3ce9e2a9cb5b3d5ce73e9eb36
donggillee-dev <donggillee94@gmail.com>
2021-01-31 19:05:44 +0900

    Programmers Lv2 점프와 순간이동 Problem Solved!!

---
+++ src/Programmers_Lv2/JumpAndTP.java
@@ -0,0 +1,19 @@
+package Programmers_Lv2;
+
+public class JumpAndTP {
+    public static void main(String[] args) {
+        System.out.println(Solution.solution(5));
+    }
+    private static class Solution {
+        private static int solution(int num) {
+            int answer = 0;
+
+            while(num > 0) {
+                if(num % 2 == 1) answer++;
+                num /= 2;
+            }
+
+            return answer;
+        }
+    }
+}
