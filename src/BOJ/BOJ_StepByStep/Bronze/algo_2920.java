692d5ae0d46806cec9a15cb7d855f15a9749d438
donggillee-dev <donggillee94@gmail.com>
2021-02-09 14:31:38 +0900

    BOJ 2920 음계 Problem Solved 걸린 시간 : 5분

---
+++ src/BOJ/BOJ_StepByStep/Bronze/algo_2920.java
@@ -0,0 +1,47 @@
+package BOJ.BOJ_StepByStep.Bronze;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class algo_2920 {
+    public static void main(String[] arg) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int prevNum1 = -1;
+        int prevNum2 = 9;
+        boolean isAscd = true;
+        boolean isDesc = true;
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        while(stk.hasMoreTokens()) {
+            int num = Integer.parseInt(stk.nextToken());
+            if(isAscd) {
+                if(prevNum1 < num) prevNum1 = num;
+                else {
+                    isAscd = false;
+                }
+            }
+
+            if(isDesc) {
+                if(prevNum2 > num) prevNum2 = num;
+                else {
+                    isDesc = false;
+                }
+            }
+        }
+
+        if(!isDesc && !isAscd) {
+            sb.append("mixed").append("\n");
+        } else if(isDesc) {
+            sb.append("descending").append("\n");
+        } else {
+            sb.append("ascending").append("\n");
+        }
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+}
