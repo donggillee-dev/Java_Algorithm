ab366133461f27a69cf3849adda0fafcea780386
donggillee-dev <donggillee94@gmail.com>
2021-02-13 17:21:17 +0900

    BOJ_2346 풍선 터뜨리기 Problem Solved!!
    주석 추가

--- src/BOJ/BOJ_StepByStep/Silver/algo_1406.java
+++ src/BOJ/BOJ_StepByStep/Silver/algo_1406.java
@@ -4,11 +4,6 @@ import java.io.*;
 import java.util.LinkedList;
 import java.util.ListIterator;
 
-//Logic
-//단순한 시뮬레이션 문제
-//다만 주로 이뤄지는 연산이 remove와 insert이기 때문에 list중에 그 두개의 연산이 빠른 LinkesList 사용
-
-//시간 초과가 나는 문제... 도저히 어디서 나는지 찾을수가 없어서 LinkedList의 시간복잡도와 관련된 문서를 다 뒤졌다...
 //
 
 
@@ -18,48 +13,7 @@ public class algo_1406 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         LinkedList<Character> editorList = new LinkedList<>();
-        String input = br.readLine();
-        int cmdLine = Integer.parseInt(br.readLine());
-        char ch;
 
-        for(int i = 0; i < input.length(); i++) {
-            editorList.add(input.charAt(i));
-        }
-        ListIterator<Character> editorIt = editorList.listIterator();
-        while(editorIt.hasNext()){
-            editorIt.next();
-        }
-
-        for(int i = 0; i < cmdLine; i++) {
-            sb.append(br.readLine());
-
-            switch(sb.charAt(0)) {
-                case 'P':
-                    ch = sb.charAt(2);
-                    editorIt.add(ch);
-                    break;
-                case 'L':
-                    if(editorIt.hasPrevious())
-                        editorIt.previous();
-                    break;
-                case 'D':
-                    if(editorIt.hasNext())
-                        editorIt.next();
-                    break;
-                case 'B':
-                    if(editorIt.hasPrevious()) {
-                        editorIt.previous();
-                        editorIt.remove();
-                    }
-                    break;
-            }
-
-            sb.setLength(0);
-        }
-
-        for(Character c : editorList) {
-            bw.write(c);
-        }
         bw.close();
         br.close();
     }
--- src/BOJ/BOJ_StepByStep/Silver/algo_2346.java
+++ src/BOJ/BOJ_StepByStep/Silver/algo_2346.java
@@ -4,6 +4,25 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+//Logic
+
+//mod를 이용하여 타겟 목표를 제대로 인덱싱 해주는 문제이다
+//이 문제의 주요 핵심은
+//1. ArrayList vs LinkedList
+//  1-1. ArrayList가 remove를 하는데 O(N)의 시간 복잡도를 가지고 LinkedList가 O(1)을 가지기에
+//      더 빨라보일 수 있지만 그렇지 않음...
+//      LinkedList가 remove를 하는데 O(1)의 시간복잡도를 가지는 경우는 맨 앞과 맨 끝인 경우
+//      결과적으로 동일하지만 get하는데 더 빠른 시간복잡도를 가지는 ArrayList를 선택하는게 맞다
+
+//2. Mod를 이요한 음수, 양수 처리
+//  2-1. 현재 위치의 풍선을 터트리면 그 위치의 오른쪽에 있는 풍선들이 당겨짐.
+//      즉 오른쪽으로 이동하게 될 경우 하나씩 당겨진 상태니까 1 빼줘야함
+//  2-2. 왼쪽으로 이동하는 경우에는 빼줄 필요없이 list의 사이즈로 moveIdx를 mod해준 값을 현재 idx에 더해주면 된다
+
+//3. 처리하고 난 값이 제대로 된 값인지 한번 더 검증
+//  3-1. 2에서 처리하고 난 현재 idx의 값이 리스트의 사이즈보다 큰 경우에는 다시한번 mod해줌
+//  3-2. 0보다 작은 음수의 경우에는 리스트의 맨 끝에서 해당 값(음수)를 더해
+
 public class algo_2346 {
     private static class BallonInfo {
         int ballonIdx;
@@ -38,12 +57,12 @@ public class algo_2346 {
 
             if(size == 0) break;
 
-            //움직임이 오른쪽이면
+            //움직임이 왼쪽이면
             if(moveIdx < 0) {
                 //타겟 idx를 움직여야하는 것에서 리스트의 사이즈를 mod해줌
                 cur_idx += (moveIdx % size);
             } else {
-                //왼쪽인 경우에는 현재 하나 터트려서 idx가 오른쪽으로 밀려있으니까 하나 빼주고 mod해줌
+                //오른쪽인 경우에는 현재 하나 터트려서 idx가 오른쪽으로 밀려있으니까 하나 빼주고 mod해줌
                 cur_idx += (moveIdx % size) - 1;
             }
 
