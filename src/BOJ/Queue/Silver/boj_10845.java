a422a76dc3f200f5a8b7759bb35858b6f82b4e31
donggillee-dev <donggillee94@gmail.com>
2021-02-15 16:28:56 +0900

    BOJ_9012 괄호 Problem solved!!
    스택으로 처리하기 보다는 char형 배열을 이용해서
    여는 괄호인 경우는 peek값 증가, 닫는 괄호인 경우에는 peek값 감소시켜서
    최종적으로 peek가 0이 되면 YES 출력
    풀이 시간 : 10분

---
+++ src/BOJ/Queue/Silver/boj_10845.java
@@ -0,0 +1,68 @@
+package BOJ.Queue.Silver;
+
+import java.io.*;
+import java.util.LinkedList;
+
+public class boj_10845 {
+    public static void main(String[] arg) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        LinkedList<Integer> list = new LinkedList<>();
+        int N = Integer.parseInt(br.readLine());
+        String input;
+        int num, size = 0;
+
+        while(N-->0) {
+            input = br.readLine();
+
+            switch(input.charAt(0)) {
+                case 'p':
+                    if(input.charAt(1) == 'u') {
+                        num = Integer.parseInt(input.substring(5));
+                        list.add(num);
+                        size++;
+                    } else {
+                        if(size == 0) {
+                            sb.append(-1).append("\n");
+                        } else {
+                            sb.append(list.get(0)).append("\n");
+                            list.remove(0);
+                            size--;
+                        }
+                    }
+                    break;
+                case 's':
+                    sb.append(size).append("\n");
+                    break;
+                case 'e':
+                    if(size == 0) {
+                        sb.append(1);
+                    } else {
+                        sb.append(0);
+                    }
+                    sb.append("\n");
+                    break;
+                case 'f':
+                    if(size == 0) {
+                        sb.append(-1);
+                    } else {
+                        sb.append(list.get(0));
+                    }
+                    sb.append("\n");
+                    break;
+                case 'b':
+                    if(size == 0) {
+                        sb.append(-1);
+                    } else {
+                        sb.append(list.get(size - 1));
+                    }
+                    sb.append("\n");
+                    break;
+            }
+        }
+        bw.write(String.valueOf(sb));
+        bw.close();
+        br.close();
+    }
+}
