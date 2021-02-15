17b12e9e857fd1934ac87535bef2443b232f2587
donggillee-dev <donggillee94@gmail.com>
2021-02-15 19:03:14 +0900

    SWEA_1493 수의 새로운 연산 Problem solved!!
    계차수열로도 풀 수 있고 단순히 반복문을 돌려서 풀수도 있는 문제,
    input 값의 범위도 그렇게 크지 않기에 복잡한 계산식보다 단순히 for문을 도는것이
    더 빠를 것이라고 생각 => 추가적으로 hashmap을 이용해서 좌표계산 효율성 증가
    풀이 시간 : 20분

---
+++ src/SWEA/D3/swea_1493.java
@@ -0,0 +1,50 @@
+package SWEA.D3;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class swea_1493 {
+    private static int[][] arr = new int[301][301];
+    private static class Info {
+        int row, col;
+        public Info(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+    }
+    public static void main(String[] arg) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        HashMap<Integer, Info> hash = new HashMap<>();
+        int num = 1;
+        for(int i = 1; i <= 300; i++) {
+            int row = 1;
+            int col = i;
+            while(col >= 1) {
+                arr[row][col] = num;
+                hash.put(num, new Info(row, col));
+                row++;
+                col--;
+                num++;
+            }
+        }
+
+        int TestCase = Integer.parseInt(br.readLine());
+        StringTokenizer stk;
+        for(int tc = 1; tc <= TestCase; tc++) {
+            stk = new StringTokenizer(br.readLine());
+            int numA = Integer.parseInt(stk.nextToken());
+            int numB = Integer.parseInt(stk.nextToken());
+
+            Info A = hash.get(numA);
+            Info B = hash.get(numB);
+
+            sb.append("#" + tc + " ").append(arr[A.row + B.row][A.col + B.col]).append("\n");
+        }
+        bw.write(String.valueOf(sb));
+        bw.close();
+        br.close();
+    }
+}
