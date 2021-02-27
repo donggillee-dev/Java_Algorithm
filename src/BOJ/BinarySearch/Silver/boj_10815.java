64f1e83840fe658407d8eade0768207e1f778a0d
donggillee-dev <donggillee94@gmail.com>
2021-02-28 05:24:57 +0900

    [boj_10815] 숫자 카드 Problem Solving start

--- src/BOJ/BinarySearch/Silver/boj_10815.java
+++ src/BOJ/BinarySearch/Silver/boj_10815.java
@@ -1,4 +1,14 @@
 package BOJ.BinarySearch.Silver;
 
+import java.io.*;
+
 public class boj_10815 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
 }
