1913f30a5a33c9096cbdae357795340006c8bef1
donggillee-dev <donggillee94@gmail.com>
2021-03-05 03:58:48 +0900

    [boj_15565] 귀여운 라이언 Problem Solved
    설명은 주석으로~

--- src/BOJ/TwoPointer/Silver/boj_15565.java
+++ src/BOJ/TwoPointer/Silver/boj_15565.java
@@ -2,7 +2,10 @@ package BOJ.TwoPointer.Silver;
 
 import java.io.*;
 import java.util.StringTokenizer;
+//Logic
 
+// Two Pointer를 가지고 처음과 끝을 이동하여주며 라이언의 개수를 목표치만큼 가지되 가장 짧은 길이의 구간을 출력해준다
+// 풀이 시간 : 30분
 public class boj_15565 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,19 +21,16 @@ public class boj_15565 {
         final int max = 987654321;
         int answer = max;
 
-        if(arr[end].equals("1")) cnt++;
-
         while(end <= N) {
             if(cnt == K) {
-                int width = end - start + 1;
-                if(max > width) answer = width;
+                int width = end - start;
+                if(answer > width) answer = width;
             }
             if(cnt < K) {
-                if(end + 1 == N) break;
-                if(end < N && arr[++end].equals("1")) cnt++;
+                if(end == N) break;
+                if(arr[end++].equals("1")) cnt++;
             } else {
-                if(arr[start].equals("1")) cnt--;
-                start++;
+                if(arr[start++].equals("1")) cnt--;
             }
         }
         if(answer == max) {
