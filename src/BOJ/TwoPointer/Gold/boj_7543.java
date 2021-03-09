40f07bbe1855f1596b5145d5d84b4fc6c36b5f15
donggillee-dev <donggillee94@gmail.com>
2021-03-10 03:27:15 +0900

    [boj_1655] 가운데를 말해요 Problem Solved
    로직은 주석으로

---
+++ src/BOJ/TwoPointer/Gold/boj_7543.java
@@ -0,0 +1,16 @@
+package BOJ.TwoPointer.Gold;
+
+import java.io.*;
+
+public class boj_7543 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+}
