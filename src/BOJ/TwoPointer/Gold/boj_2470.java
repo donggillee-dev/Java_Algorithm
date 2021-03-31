39d20091011849cdce37553274910aefe56db143
donggillee-dev <donggillee94@gmail.com>
2021-03-31 21:48:26 +0900

    [boj_2470] 두 용액 Problem solving start
    로직은 주석으로

---
+++ src/BOJ/TwoPointer/Gold/boj_2470.java
@@ -0,0 +1,11 @@
+package BOJ.TwoPointer.Gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_2470 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
