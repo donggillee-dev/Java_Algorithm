64a5a5ad09bfb5a9625ede715d192150f69327ec
donggillee-dev <donggillee94@gmail.com>
2021-04-04 20:00:23 +0900

    [Programmers_Lv2] 방금 그곡 Problem Solved
    code refactor

--- src/Programmers/Programmers_Lv2/RecentMusic.java
+++ src/Programmers/Programmers_Lv2/RecentMusic.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class RecentMusic {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        String m = "ZZZ";
+        String m = "ABC";
         String[] musicinfos = {
                 "12:00,12:14,HELLO,C#DEFGAB",
                 "13:00,13:05,WORLD,ABCDEF"
@@ -16,30 +16,15 @@ public class RecentMusic {
         public String solution(String m, String[] musicinfos) {
             HashMap<String, String> hash = new HashMap<>();
             StringBuilder sb = new StringBuilder();
-            StringBuilder tmp = new StringBuilder();
+            StringBuilder ansTitle = new StringBuilder("(None)");
             int[] pi = new int[m.length()];
-
-            sb.append(m);
-            hash.put("C#", "c");
-            hash.put("D#", "d");
-            hash.put("F#", "f");
-            hash.put("G#", "g");
-            hash.put("A#", "a");
-
-            for(String key : hash.keySet()) {
-                tmp.append(sb.toString().replaceAll(key, hash.get(key)));
-                sb.setLength(0);
-                sb.append(tmp);
-                tmp.setLength(0);
-            }
-
-            m = sb.toString();
-            makeTable(sb.toString(), pi);
-            sb.setLength(0);
-
             int ansLength = 0;
-            StringBuilder ansTitle = new StringBuilder();
-            ansTitle.append("(None)");
+
+            initHash(hash);
+
+            m = changeStr(hash, m);
+            makeTable(sb.toString(), pi);
+
             for(String line : musicinfos) {
                 String[] info = line.split(",");
                 String[] start = info[0].split(":");
@@ -48,19 +33,9 @@ public class RecentMusic {
                 int endTime = Integer.parseInt(end[0]) * 3600 + Integer.parseInt(end[1]) * 60;
                 int playTime = (endTime - startTime) / 60;
 
+                info[3] = changeStr(hash, info[3]);
+
                 sb.append(info[3]);
-
-                for(String key : hash.keySet()) {
-                    tmp.append(sb.toString().replaceAll(key, hash.get(key)));
-                    sb.setLength(0);
-                    sb.append(tmp);
-                    tmp.setLength(0);
-                }
-
-                String hashStr = sb.toString();
-                sb.setLength(0);
-
-                sb.append(hashStr);
                 int length = sb.length();
 
                 if(length < playTime) {
@@ -68,12 +43,12 @@ public class RecentMusic {
                     int rem = playTime % length;
 
                     while(cnt --> 0) {
-                        sb.append(hashStr);
+                        sb.append(info[3]);
                     }
 
                     if(rem != 0) {
                         for(int i = 0; i < rem; i++) {
-                            sb.append(hashStr.charAt(i));
+                            sb.append(info[3].charAt(i));
                         }
                     }
                 } else if(length > playTime) {
@@ -92,6 +67,27 @@ public class RecentMusic {
             }
 
             return ansTitle.toString();
+        }
+        private static void initHash(HashMap<String, String> hash) {
+            hash.put("C#", "c");
+            hash.put("D#", "d");
+            hash.put("F#", "f");
+            hash.put("G#", "g");
+            hash.put("A#", "a");
+        }
+
+        private static String changeStr(HashMap<String, String> hash, String origin) {
+            StringBuilder sb = new StringBuilder(origin);
+            StringBuilder tmp = new StringBuilder();
+
+            for(String key : hash.keySet()) {
+                tmp.append(sb.toString().replaceAll(key, hash.get(key)));
+                sb.setLength(0);
+                sb.append(tmp);
+                tmp.setLength(0);
+            }
+
+            return sb.toString();
         }
         private static void makeTable(String pattern, int[] pi) {
             int j = 0, length = pattern.length();
