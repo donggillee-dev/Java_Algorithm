c1f757e1579a5f3146f870981d72991b7285ef4c
donggillee-dev <donggillee94@gmail.com>
2021-04-04 20:05:19 +0900

    [Programmers_Lv2] 방금 그곡 Problem Solved
    주석 추가

--- src/Programmers/Programmers_Lv2/RecentMusic.java
+++ src/Programmers/Programmers_Lv2/RecentMusic.java
@@ -1,6 +1,16 @@
 package Programmers.Programmers_Lv2;
 import java.util.*;
 
+//Logic
+//내 아이디어는
+//  1. 문자열에 섞여있는 #을 어떻게든 해주고싶다
+//  2. 내가 들은 멜로디가 라디오에서 들려주는 멜로디 ( 총 플레이시간 ) 의 부분 문자열인지 체크 => KMP 알고리즘
+
+// 첫번째 아이디어를 위해서는 hashMap을 이용해서 C#, A#, 등등 #이 붙은 음표들을 replaceAll로 처리해주었다
+// 그 과정에서 String자체게 문자열 조작 연산을 해주면 메모리 재할당, 삭제에 의해 시간 복잡도가 증가하므로 StringBuilder를 이요
+// musicinfo배열에 대해서는 split 메소드릉 이용해서 플레이시간과 총 플레이 시간에 따른 멜로디를 재구성하였다
+// 재구성된 멜로디 문자열(문자열 전체)과 비교대상인 내가 들은 멜로디(부분 문자열)을 KMP 알고리즘으로 비교해준다
+
 public class RecentMusic {
     public static void main(String[] args) {
         Solution sol = new Solution();
