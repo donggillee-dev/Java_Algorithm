d88cc31fca3bbf8710105eaa2a9f0097718f5fef
donggillee-dev <donggillee94@gmail.com>
2021-04-06 21:12:48 +0900

    [Programmers] N진수 게임 Problem Solving...

---
+++ src/Programmers/Programmers_Lv2/NGame.java
@@ -0,0 +1,11 @@
+package Programmers.Programmers_Lv2;
+
+public class NGame {
+    public static void main(String[] args) {
+
+    }
+    public String solution(int n, int t, int m, int p) {
+        String answer = "";
+        return answer;
+    }
+}
