54fd4436cd867e349b6f3136733ad4e244656998
donggillee-dev <donggillee94@gmail.com>
2021-04-10 18:07:43 +0900

    [boj_11438] LCA2 Problem Solved 다시 풀어보자

---
+++ src/BOJ/LCA/Platinum/boj_11438.java
@@ -0,0 +1,11 @@
+package BOJ.LCA.Platinum;
+
+//Logic
+//LCA 알고리즘이지만 노드의 개수가 많은 관게로 각 노드의 2^x번쨰 부모를 저장하는 방식으로 시간을 단축할 수 있다
+//parents[i][j] = i번째 노드의 2^j번째 부모
+
+//다시 풀어보자
+
+public class boj_11438 {
+
+}
