e96649f2bd38f5b7243f7935e5c6256af2545080
donggillee-dev <donggillee94@gmail.com>
2021-04-13 16:54:05 +0900

    [boj_2239] 스도쿠 Problem Solved 로직은 주석으로

--- src/BOJ/BackTracking/Gold/boj_2239.java
+++ src/BOJ/BackTracking/Gold/boj_2239.java
@@ -8,6 +8,86 @@ import java.util.ArrayList;
 //그냥 백트레킹으로 풀면 되는 문제
 //일일히 하나씩 넣어보면서 조건 체크 => 백트레킹
 //풀이 시간 : 40분
+
+//더 빠른 코드가 있으니까 참고해보자
+
+//public class Main {
+//    private static int[][] sudoku;
+//    static List<Pair2239> list;
+//    static int zero = 0;
+//    private static int[] row;
+//    private static int[] col;
+//    private static int[][] box;
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        list = new ArrayList<Pair2239>();
+//
+//        sudoku = new int[9][9];
+//        row = new int[9];
+//        col = new int[9];
+//        box = new int[3][3];
+//
+//        for (int i = 0; i < 9; i++) {
+//            String str = br.readLine();
+//            for (int j = 0; j < 9; j++) {
+//                int tmp = (int) (str.charAt(j) - '0');
+//                sudoku[i][j] = tmp;
+//                if (tmp == 0) {
+//                    list.add(new Pair2239(i, j));
+//                    zero++;
+//                } else {
+//                    row[i] |= (1 << tmp);
+//                    col[j] |= (1 << tmp);
+//                    box[i / 3][j / 3] |= (1 << tmp);
+//                }
+//            }
+//        }
+//
+//        playSudoku(0);
+//
+//    }
+//
+//    private static void playSudoku(int cnt) {
+//
+//        if (cnt == zero) {
+//            for (int i = 0; i < 9; i++) {
+//                for (int j = 0; j < 9; j++) {
+//                    System.out.print(sudoku[i][j]);
+//                }
+//                System.out.println();
+//            }
+//            System.exit(0);
+//        }
+//
+//        Pair2239 pair = list.get(cnt);
+//
+//        for (int i = 1; i <= 9; i++) {
+//            if ((row[pair.x] & (1 << i)) == 0 && (col[pair.y] & (1 << i)) == 0
+//                    && (box[pair.x / 3][pair.y / 3] & (1 << i)) == 0) {
+//                row[pair.x] |= (1 << i);
+//                col[pair.y] |= (1 << i);
+//                box[pair.x / 3][pair.y / 3] |= (1 << i);
+//                sudoku[pair.x][pair.y] = i;
+//                playSudoku(cnt + 1);
+//                row[pair.x] ^= (1 << i);
+//                col[pair.y] ^= (1 << i);
+//                box[pair.x / 3][pair.y / 3] ^= (1 << i);
+//            }
+//        }
+//    }
+//
+//    static class Pair2239 {
+//        int x, y;
+//
+//        public Pair2239(int x, int y) {
+//            super();
+//            this.x = x;
+//            this.y = y;
+//        }
+//
+//    }
+//}
 public class boj_2239 {
     private static boolean possRow(int r) {
         boolean[] visited = new boolean[10];
