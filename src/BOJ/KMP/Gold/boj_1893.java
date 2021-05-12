44c00c271d27eb5f919c6f6db15932fddce5ce9b
donggillee-dev <donggillee94@gmail.com>
2021-05-12 18:06:38 +0900

    [boj_1893] 시저 암호 Problem solving... 로직은 주석으로

---
+++ src/BOJ/KMP/Gold/boj_1893.java
@@ -0,0 +1,21 @@
+package BOJ.KMP.Gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//Logic
+//Solving...
+public class boj_1893 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        while(t --> 0) {
+            String a = br.readLine();
+            String w = br.readLine();
+            String s = br.readLine();
+            int length = w.length();
+
+        }
+    }
+}
