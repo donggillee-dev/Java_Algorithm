d5622cfbbed14ec6d35eed575a87aea9e9b2a6f6
donggillee-dev <donggillee94@gmail.com>
2021-07-01 22:53:41 +0900

    [boj_2056] 작업 Problem solving start 로직은 주석으로

---
+++ src/BOJ/TopologySort/Gold/boj_2056.java
@@ -0,0 +1,11 @@
+package BOJ.TopologySort.Gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_2056 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
