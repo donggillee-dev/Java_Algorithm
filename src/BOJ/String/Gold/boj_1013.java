b18cf87a3a6be2b450e99721497b721d070b5fa9
donggillee-dev <donggillee94@gmail.com>
2021-07-15 21:33:40 +0900

    [boj_1013] Contact Problem resolved 로직은 주석으로

--- src/BOJ/String/Gold/boj_1013.java
+++ src/BOJ/String/Gold/boj_1013.java
@@ -3,26 +3,25 @@ import java.io.*;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
+//Pattern, Matcher 클래스를 사용하면 되는 문제
+//Pattern.compile로 패턴 클래스 생성
+//Matches 클래스로 문자열에 대해 pattern.matcher로 가져옴
+
+//다시 풀어본 풀이 시간 : 5분
+
 public class boj_1013 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder tmp;
         StringBuilder sb = new StringBuilder();
-
         Pattern pattern = Pattern.compile("((100+1+)|(01))+");
-        int N = Integer.parseInt(br.readLine());
-        for(int i = 0; i < N; i++) {
-            tmp = new StringBuilder();
-            tmp.append(br.readLine());
-            Matcher matcher = pattern.matcher(tmp.toString());
+        int n = Integer.parseInt(br.readLine());
 
+        for(int i = 0; i < n; i++) {
+            Matcher matcher = pattern.matcher(br.readLine());
             if(matcher.matches()) sb.append("YES").append("\n");
             else sb.append("NO").append("\n");
         }
 
-        bw.write(sb.toString());
-        bw.close();
-        br.close();
+        System.out.print(sb);
     }
 }
