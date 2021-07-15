6d209732a9f9b6f9d5cf3ed5b03c8a236feec309
donggillee-dev <donggillee94@gmail.com>
2021-07-15 21:28:06 +0900

    [boj_14600] 샤워실 바닥 깔기(Small) Problem resolved 로직은 주석으로

--- src/BOJ/DivideAndConquer/Gold/boj_14600.java
+++ src/BOJ/DivideAndConquer/Gold/boj_14600.java
@@ -11,51 +11,53 @@ import java.util.*;
 
 //문제 좀 잘 읽자... 여러 답 중에 하나만 나오면 되는 문제이다.
 
+//다시 풀어봄 풀이 시간 : 25분
+
 public class boj_14600 {
-    private static int num = 0;
-    private static int[][] booth;
-    private static boolean isPossible(int width, int r, int c) {
-        for(int i = r; i < r + width; i++) {
-            for(int j = c; j < c + width; j++) {
+    private static int num;
+    private static boolean isPossible(int row, int col, int size, int[][] booth) {
+        for(int i = row; i < row + size; i++) {
+            for(int j = col; j < col + size; j++) {
                 if(booth[i][j] != 0) return false;
             }
         }
+
         return true;
     }
+    private static void recur(int row, int col, int size, int[][] booth) {
+        int divSize = size / 2;
 
-    private static void solution(int size, int r, int c) {
         num++;
 
-        int divSize = size / 2;
-        if(isPossible(divSize, r + divSize, c))
-            booth[r + divSize][c + divSize - 1] = num;
-        if(isPossible(divSize, r + divSize, c + divSize))
-            booth[r + divSize][c + divSize] = num;
-        if(isPossible(divSize, r, c))
-            booth[r + divSize - 1][c + divSize - 1] = num;
-        if(isPossible(divSize, r, c + divSize))
-            booth[r + divSize - 1][c + divSize] = num;
+        if(isPossible(row, col, divSize, booth)) //왼쪽 위
+            booth[row + divSize - 1][col + divSize - 1] = num;
+        if(isPossible(row + divSize, col, divSize, booth)) //왼쪽 아래
+            booth[row + divSize][col + divSize - 1] = num;
+        if(isPossible(row, col + divSize, divSize, booth)) //오른쪽 위
+            booth[row + divSize - 1][col + divSize] = num;
+        if(isPossible(row + divSize, col + divSize, divSize, booth)) //오른쪽 아래
+            booth[row + divSize][col + divSize] = num;
 
-        if(size == 2) return;
+        if(divSize == 1) return;
 
-        solution(divSize, r + divSize, c);
-        solution(divSize, r + divSize, c + divSize);
-        solution(divSize, r, c);
-        solution(divSize, r, c + divSize);
+        recur(row, col, divSize, booth);
+        recur(row + divSize, col, divSize, booth);
+        recur(row, col + divSize, divSize, booth);
+        recur(row + divSize, col + divSize, divSize, booth);
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-        int size = (int)Math.pow(2, N);
+        int k = Integer.parseInt(br.readLine());
+        int size = (int)Math.pow(2, k);
+        int[][] booth = new int[size][size];
         StringTokenizer stk = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(stk.nextToken());
+        int y = Integer.parseInt(stk.nextToken());
 
-        booth = new int[size][size];
-        int col = Integer.parseInt(stk.nextToken()), row = size - Integer.parseInt(stk.nextToken());
+        booth[size - y][x - 1] = -1;
 
-        booth[row][col - 1] = -1;
-
-        solution(size, 0, 0);
+        recur(0, 0, size, booth);
 
         for(int i = 0; i < size; i++) {
             for(int j = 0; j < size; j++) {
@@ -63,6 +65,7 @@ public class boj_14600 {
             }
             sb.append("\n");
         }
+
         System.out.print(sb);
     }
 }
