30a2347cf16ab849ca534eaa7b44d6595370ea4d
donggillee-dev <donggillee94@gmail.com>
2021-07-18 16:36:11 +0900

    [boj_12878] Block Problem solving... 로직은 주석으로

---
+++ src/BOJ/DivideAndConquer/Gold/boj_12878.java
@@ -0,0 +1,17 @@
+package BOJ.DivideAndConquer.Gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_12878 {
+    private static int ans;
+    private static void recur(int red, int yellow, int green, int orange) {
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(ans);
+    }
+}
