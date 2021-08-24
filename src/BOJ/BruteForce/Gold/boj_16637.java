80812bf2736fe0c9459cfa8214b323520507c1db
donggillee-dev <donggillee94@gmail.com>
2021-08-24 10:31:31 +0900

    [Programmers_Kakao_InternShip_2021] 거리두기 확인하기 풀이 완료

---
+++ src/BOJ/BruteForce/Gold/boj_16637.java
@@ -0,0 +1,11 @@
+package BOJ.BruteForce.Gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_16637 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
