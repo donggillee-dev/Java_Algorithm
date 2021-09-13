b95d4712bf41ea5ed4d3a19395b4f71c921f671d
donggillee-dev <donggillee94@gmail.com>
2021-09-13 18:28:09 +0900

    [boj_4195] 친구 네트워크 solving

---
+++ src/BOJ/Hash/Gold/boj_4195.java
@@ -0,0 +1,27 @@
+package BOJ.Hash.Gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class boj_4195 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk;
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+
+        while(t --> 0) {
+            HashMap<String, Integer> hash = new HashMap<>();
+            int n = Integer.parseInt(br.readLine());
+
+            for(int i = 0; i < n; i++) {
+                stk = new StringTokenizer(br.readLine());
+
+            }
+        }
+        System.out.print(sb);
+    }
+}
