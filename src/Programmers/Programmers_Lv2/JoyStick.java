6676361f44a548fc3d718d0ecb76c585ca673635
donggillee-dev <donggillee94@gmail.com>
2021-09-22 18:08:23 +0900

    [Programmers_Lv2] 조이 스틱 다시 풀기 완료

--- src/Programmers/Programmers_Lv2/JoyStick.java
+++ src/Programmers/Programmers_Lv2/JoyStick.java
@@ -2,29 +2,20 @@ package Programmers.Programmers_Lv2;
 
 public class JoyStick {
     private static int solution(String name) {
-        int answer = 0;
-        int len = name.length();
+        int answer = 0, length = name.length();
 
-        // 제일 짧은 좌, 우 이동은 그냥 맨 오른쪽으로 이동할 때
-        int min = len - 1;
+        int min = length - 1;
 
-        for (int i = 0; i < len; i++) {
-            // 조이스틱 상, 하 이동
-            char c = name.charAt(i);
-            int mov = (c - 'A' < 'Z' - c + 1) ? (c - 'A') : ('Z' - c + 1);
-            answer += mov;
+        for(int i = 0; i < length; i++) {
+            answer += (name.charAt(i) - 'A') > ('Z' - name.charAt(i) + 1) ? ('Z' - name.charAt(i) + 1) : (name.charAt(i) - 'A');
 
-            // 조이스틱 좌, 우 이동
-            int nextIndex = i + 1;
-            // 다음 단어가 A이고, 단어가 끝나기 전까지 nextIndex 증가
-            while (nextIndex < len && name.charAt(nextIndex) == 'A')
-                nextIndex++;
+            int index = i + 1;
 
-            min = Math.min(min, (i * 2) + len - nextIndex);
+            while(index < length && name.charAt(index) == 'A') index++;
+
+            min = Math.min(min, (i * 2 + (length - index)));
         }
 
-        answer += min;
-
-        return answer;
+        return answer + min;
     }
 }
