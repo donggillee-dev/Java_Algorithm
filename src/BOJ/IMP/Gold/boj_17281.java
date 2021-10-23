1e9bf62dfe20aef088914b23cadb7628201548e6
donggillee-dev <donggillee94@gmail.com>
2021-10-24 00:36:41 +0900

    [boj_17281] 야구 문제 풀이 완료

--- src/BOJ/IMP/Gold/boj_17281.java
+++ src/BOJ/IMP/Gold/boj_17281.java
@@ -19,39 +19,93 @@ import java.io.*;
 import java.util.*;
 
 public class boj_17281 {
-	private static int N, ans = -987654321;
+	private static int N, ans = 0;
 	private static int[][] infoArr;
-	
+
 	private static int stoi(String str) {
 		return Integer.parseInt(str);
 	}
-	
+
 	private static int getScore(int[] order) {
-		int score = 0;
-		
-		for(int inning = 0; inning < N; inning++) {
-			int strikeCnt = 0, curOrder = 0;
-			
-			while(strikeCnt < 3) {
+		int score = 0, orderIdx = 0;
+
+		for (int inning = 0; inning < N; inning++) {
+			int strikeCnt = 0;
+			boolean[] base = { false, false, false};
+
+			while (strikeCnt < 3) {
+				int curOrder = order[orderIdx]; // 현재 선수 번호
+				int action = infoArr[inning][curOrder]; // 현재 선수가 일으키는 일
+
+				switch (action) {
+					case 0: {
+						strikeCnt++;
+						break;
+					}
+					
+					case 1: {
+						if(base[2]) score++;
+						
+						base[2] = base[1];
+						base[1] = base[0];
+						base[0] = true;
+						break;
+					}
+					
+					case 2: {
+						for(int i = 1; i < 3; i++) { //2루 ~ 3루 사람들 들여보냄
+							if(base[i]) {
+								score++;
+							}
+							base[i] = false;
+						}
+						base[2] = base[0]; //1루에 있는 사람은 3루로
+						base[0] = false;
+						base[1] = true; //타자는 2루로
+						break;
+					}
+					
+					case 3: {
+						for(int i = 0; i < 3; i++) { //1루 ~ 3루 사람들 모두 들어옴
+							if(base[i]) {
+								score++;
+							}
+							base[i] = false;
+						}
+						base[2] = true; //타자 3루에 배치
+						break;
+					}
+					
+					case 4: {
+						for(int i = 0; i < 3; i++) { //베이스에 있는 모두 주자들 in
+							if(base[i]) {
+								score++;
+							}
+							base[i] = false;
+						}
+						score++; //타자도 in
+						break;
+					}
 				
-				curOrder = (curOrder + 1) % 9;
+				}
+				orderIdx = (orderIdx + 1) % 9;
 			}
 		}
-		
+
 		return score;
 	}
-	
+
 	private static void dfs(int depth, boolean[] visited, int[] order) {
-		if(depth >= 9) {
+		if (depth >= 9) {
 			ans = Math.max(ans, getScore(order));
 			return;
 		}
-		
-		if(depth == 3) {
+
+		if (depth == 3) {
 			dfs(depth + 1, visited, order);
 		} else {
-			for(int i = 0; i < 9; i++) {
-				if(!visited[i]) {
+			for (int i = 0; i < 9; i++) {
+				if (!visited[i]) {
 					visited[i] = true;
 					order[depth] = i;
 					dfs(depth + 1, visited, order);
@@ -60,24 +114,26 @@ public class boj_17281 {
 				}
 			}
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer stk;
 		N = stoi(br.readLine());
 		infoArr = new int[N][9];
-		int[] order = {-1, -1, -1, 0, -1, -1, -1, -1, -1};
-		boolean[] visited = {true, false, false, false, false, false, false, false, false};
-		
-		for(int inning = 0; inning < N; inning++) {
+		int[] order = { -1, -1, -1, 0, -1, -1, -1, -1, -1 };
+		boolean[] visited = { true, false, false, false, false, false, false, false, false };
+
+		for (int inning = 0; inning < N; inning++) {
 			stk = new StringTokenizer(br.readLine());
-			for(int idx = 0; idx < 9; idx++) {
+			for (int idx = 0; idx < 9; idx++) {
 				infoArr[inning][idx] = stoi(stk.nextToken());
 			}
 		}
-		
+
 		dfs(0, visited, order);
+		
+		System.out.println(ans);
 	}
 }
